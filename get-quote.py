61e6ac27d52356eb7cb97d63951e40251e4eb0be
farhana01 <farhanalima01@gmail.com>
2020-10-22 19:23:35 +0600

    can add quote in text file

--- get-quote.py
+++ get-quote.py
@@ -1,13 +1,28 @@
 import random
+
+
 def primary():
   # print("Keep it logically awesome.")
 
   f = open("quotes.txt")
   quotes = f.readlines()
   f.close()
-  last = 13
+  last = len(quotes)-1
   rnd = random.randint(0, last)
   print(quotes[rnd])
+  
+
+def addQuote():
+    quote= input("Write the quote you want to add...\n")
+    file_object = open('quotes.txt', 'a')
+    file_object.write("\n"+ quote)
+    file_object.close()
+    
+
 
 if __name__== "__main__":
-  primary()
+    ans = input("add more quote? Yes or no?\n")
+    if ans.lower() == "yes":
+        addQuote()
+    primary()
+
